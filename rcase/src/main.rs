e300f9fd7bac36bff5e03922bc5aeda67751f269
LordMZTE <lord@mzte.de>
2020-09-03 20:45:15 +0200

    init

---
+++ rcase/src/main.rs
@@ -0,0 +1,41 @@
+use rcase::randomize_case;
+use std::io;
+use std::env::args;
+
+fn main() {
+    let mut rng = rand::thread_rng();
+    let mut input = args();
+
+    if input.len() > 1 {
+        let mut buf = String::new();
+
+        //remove first one.
+        input.next();
+        loop {
+            match input.next() {
+                Some(w) => {
+                    buf.push_str(w.as_str());
+                    buf.push(' ');
+                },
+                None => {
+                    buf.pop();
+                    break;
+                },
+            }
+        }
+
+        println!("{}", randomize_case(buf, &mut rng))
+    } else {
+        loop {
+            let mut buf = String::new();
+            if let Err(_) = io::stdin().read_line(&mut buf) {
+                continue;
+            }
+
+            if buf.is_empty() {
+                break;
+            }
+            print!("{}", randomize_case(buf, &mut rng));
+        }
+    }
+}
